ce0315365dccece2b2279a50fd2900049b9a9be3
Jiayuan Gu <jigu@ucsd.edu>
2018-10-26 12:53:43 -0700

    clear syntax for solver

--- maskrcnn_benchmark/solver/build.py
+++ maskrcnn_benchmark/solver/build.py
@@ -6,17 +6,18 @@ from .lr_scheduler import WarmupMultiStepLR
 
 def make_optimizer(cfg, model):
     params = []
+    base_lr = cfg.SOLVER.BASE_LR
     for key, value in model.named_parameters():
         if not value.requires_grad:
             continue
-        lr = cfg.SOLVER.BASE_LR
+        lr = base_lr
         weight_decay = cfg.SOLVER.WEIGHT_DECAY
         if "bias" in key:
-            lr = cfg.SOLVER.BASE_LR * cfg.SOLVER.BIAS_LR_FACTOR
+            lr = base_lr * cfg.SOLVER.BIAS_LR_FACTOR
             weight_decay = cfg.SOLVER.WEIGHT_DECAY_BIAS
         params += [{"params": [value], "lr": lr, "weight_decay": weight_decay}]
 
-    optimizer = torch.optim.SGD(params, lr, momentum=cfg.SOLVER.MOMENTUM)
+    optimizer = torch.optim.SGD(params, base_lr, momentum=cfg.SOLVER.MOMENTUM)
     return optimizer
 
 
